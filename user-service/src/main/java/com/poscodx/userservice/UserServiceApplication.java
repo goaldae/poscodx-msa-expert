f470cb7668b75ca2cb2a9a4c5344ab8fca7d72e2
goaldae <goaldae@naver.com>
2023-09-30 17:54:38 +0900

    Apply service name instade of IP Address for order service

--- user-service/src/main/java/com/poscodx/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/poscodx/userservice/UserServiceApplication.java
@@ -3,6 +3,7 @@ package com.poscodx.userservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,7 +23,8 @@ public class UserServiceApplication {
 	}
 
 	@Bean
-	public RestTemplate getRestTemplate(){
+	@LoadBalanced //서비스를 호출할 때 IP address가 아닌 서비스 이름으로 하기 위해
+	public RestTemplate getRestTemplate(){ //각 서비스간 Rest API Call을 하기 위해 선언
 		return new RestTemplate();
 
 	}
